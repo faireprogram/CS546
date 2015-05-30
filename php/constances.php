b92c2985056632cc2f7017d9e19d41d7b00914f4
faireprogram <michael.zhangwen@yahoo.com>
2015-05-29 23:46:07 -0400

    structure

---
+++ php/constances.php
@@ -0,0 +1,6 @@
+<?php
+
+define("DATA_DIR", dirname(__FILE__)."\data");
+define("DATA_CHAT_DIR", dirname(__FILE__)."\data\chat");
+
+?>
--- php/test.php
+++ php/test.php
@@ -1,4 +1,37 @@
 <?php
 
-phpinfo();
+require_once './constances.php';
+
+$before = microtime(true);
+$current_folder = dirname(__FILE__)."/test";
+
+if( !file_exists($current_folder) ) {
+	// if not exit, then create a new file
+	mkdir($current_folder,0700);
+}
+
+function create_file($name, $folder) {
+	$handle = fopen($folder."/".$name, "w");
+	fwrite($handle, $name);
+	fclose($handle);
+}
+
+echo file_exists($current_folder."/name91309txt")."</BR>";
+
+$now = microtime(true);
+
+
+echo $now - $before."</BR>";
+
+echo DATA_CHAT_DIR;
+
+// $count = 100000;
+
+// while($count > 0) {
+// 	create_file("name".$count."txt", $current_folder);
+// 	$count--;	
+// }
+
+
+
 ?>
