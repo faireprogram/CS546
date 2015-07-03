246743bbcb83baeaff03a3f337e4536b0c8de335
faireprogram <michael.zhangwen@yahoo.com>
2015-07-03 14:39:04 -0400

    fix register errors

--- php/utils/StringHelper.php
+++ php/utils/StringHelper.php
@@ -7,4 +7,20 @@ function pc($s) {
 function v4() {
 	return sprintf('%04x', mt_rand(0, 0xffff));
 }
+
+function isValidYear($a) {
+	if(!$a || !preg_match("/\d{4}/i", $a)) {
+		return false;
+	}
+	
+	$a = intval($a);
+	$minum = date("Y")-100;
+	$maxnum = date("Y");
+	
+	if($a >= $minum &&  $a <= $maxnum) {
+		return true;
+	} else {
+		return false;
+	}
+}
 ?>
